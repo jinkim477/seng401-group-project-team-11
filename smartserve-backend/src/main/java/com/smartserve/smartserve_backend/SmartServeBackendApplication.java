ec05f8426af578c62b880d56d559504311f0a7e7
Jin Kim <jinkim477@gmail.com>
2025-02-22 13:28:51 -0700

    added initialized spring boot directory

---
+++ smartserve-backend/src/main/java/com/smartserve/smartserve_backend/SmartServeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartserve.smartserve_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartServeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartServeBackendApplication.class, args);
+	}
+
+}
